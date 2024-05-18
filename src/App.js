bad55cf75942bbc7da5e5d76bc87c096a3d4a0b5
ROUSHAN SINGH <rou@gul.com>
2024-05-18 10:26:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Item  from './components/Item'
 
 function App() {
   return (
-    <div className="App">
-   hello jee
-   
+
+    <div>
+      <Item></Item>
+    
+    <div className="App">Hello Jee </div>
     </div>
   );
 }
---
+++ src/sahil.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "sahil badmash h" << endl;
+    cout << "sejal bht jYd BADMASH H"  <<endl;
+}
