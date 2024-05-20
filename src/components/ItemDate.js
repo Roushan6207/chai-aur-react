d96bbe1ff1c0745c4bfdbb5b08a5b61822bf70ad
ROUSHAN SINGH <rou@gul.com>
2024-05-20 10:45:38 +0530

    hh

---
+++ src/components/ItemDate.js
@@ -0,0 +1,13 @@
+import './ItemDate.css';
+
+
+function ItemDate(){
+    return (<div>
+        <div>20</div>
+        <div>June</div>
+        <div>1998</div>
+    </div>);
+
+}
+
+export default ItemDate;
