06c7396824e55c9a210211180e31800a11717e13
ROUSHAN SINGH <rou@gul.com>
2024-05-20 11:08:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Item  from './components/Item'
+import ItemDate from './components/ItemDate';
 
 function App() {
   return (
 
+
     <div>
       <Item></Item>
+       <ItemDate></ItemDate>
     
     <div className="App">Hello Jee </div>    
     </div>
